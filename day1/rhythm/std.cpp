81d538699378804e49921e773b1cc630eac9741a
Shunpower <1024357726@qq.com>
2025-03-08 02:27:47 +0800

    modified:   The 4th Simons Cup Solution.md 	modified:   day1/rhythm/statement/zh-cn.md 	modified:   day1/rhythm/std.cpp 	modified:   day1/rhythm/tables/explain.pyinc

--- day1/rhythm/std.cpp
+++ day1/rhythm/std.cpp
@@ -59,25 +59,132 @@ namespace Shun{
 	}
 }
 using namespace Shun;
+// ll remind(int l,int r){
+// 	cout<<"? "<<l<<" "<<r<<endl;
+// 	ll x;
+// 	cin>>x;
+// 	return x;
+// }
 int tc;
-vector <int> rhythm(int n,int q,int c){
-
+vector <ll> ans;
+bool type[2010];
+struct interval{
+	int l,r;
+	ll sum;//-1e18 means negative
+};
+vector <interval> p;
+void cover(int l,int r,ll x){
+	// cout<<l<<" "<<r<<" "<<x<<endl;
+	fr1(i,l,r) ans[i-1]=x;
 }
-int main(){
-#ifdef Shun
-	freopen(".in","r",stdin);
-	freopen(".out","w",stdout);
-#endif
-	ios::sync_with_stdio(false);
-	cin>>tc;
-	while(tc--){
-		int n;
-		cin>>n;
-		vector <int> ans=rhythm(n,2*n,0);
-		cout<<"! ";
-		for(auto i:ans) cout<<i<<" ";
-		cout<<endl;
+void lefmerge(int id1,int id2,int id3,ll x){
+	cover(p[id2].l,p[id2].r,0);
+	cover(p[id2].r,p[id2].r,x-(p[id1].sum+p[id3].sum));
+	p[id3].sum=x;
+	p[id3].l=p[id1].l;
+	p.erase(p.begin()+id2);
+	p.erase(p.begin()+id1);
+}
+void rigmerge(int id1,int id2,int id3,ll x){
+	// cout<<"!"<<p[id1].sum<<" "<<p[id3].sum<<" "<<x<<endl;
+	cover(p[id2].l,p[id2].r,0);
+	cover(p[id2].r,p[id2].r,x-(p[id1].sum+p[id3].sum));
+	p[id1].sum=x;
+	p[id1].r=p[id3].r;
+	p.erase(p.begin()+id3);
+	p.erase(p.begin()+id2);
+}
+vector <ll> rhythm(int n,int q,int c){
+	p.clear();
+	ans.clear();
+	ans.resize(n);
+	fr1(i,1,n){
+		ll x=remind(i,i);
+		type[i]=x;
+		if(x) ans[i-1]=x;
 	}
-	ET;
+	int lst=1;
+	ll sum=0;
+	fr1(i,1,n){
+		sum+=ans[i-1];
+		if(type[i]!=type[i+1]||i==n){
+			p.push_back({lst,i,(type[i]?sum:(ll)-1e18)});
+			lst=i+1;
+			sum=0;
+		}
+	}
+	if((p.begin()->sum)==-1e18){
+		cover(p.begin()->l,p.begin()->r,0);
+		p.erase(p.begin());
+	}
+	if(p.empty()) return ans;
+	if(p.back().sum==-1e18){
+		cover(p.back().l,p.back().r,0);
+		p.pop_back();
+	}
+	if(p.empty()) return ans;
+	while(p.size()>1){
+		// for(auto i:p) cout<<i.l<<" "<<i.r<<" "<<i.sum<<endl;
+		int pos=-1;
+		fr1(i,0,(int)p.size()-1){
+			if(p[i].sum!=-1e18){
+				if(~pos){
+					if(p[i].sum<p[pos].sum) pos=i;
+				}
+				else pos=i;
+			}
+		}
+		ll x=-1e18,y=-1e18;
+		// cout<<"!"<<pos<<endl;
+		if(pos!=0) x=remind(p[pos-2].l,p[pos].r);
+		if(pos!=(int)p.size()-1) y=remind(p[pos].l,p[pos+2].r);
+		if(x==-1e18){
+			if(y!=p[pos+2].sum) rigmerge(pos,pos+1,pos+2,y);
+			else{
+				// cout<<"?"<<endl;
+				cover(p[pos+1].l,p[pos+1].r,0);
+				cover(p[pos+1].r,p[pos+1].r,-p[pos].sum);
+				p.erase(p.begin()+pos+1);
+				p.erase(p.begin()+pos);
+			}
+		}
+		else if(y==-1e18){
+			if(x!=p[pos-2].sum) lefmerge(pos-2,pos-1,pos,x);
+			else{
+				cover(p[pos-1].l,p[pos-1].r,0);
+				cover(p[pos-1].l,p[pos-1].l,-p[pos].sum);
+				p.pop_back();
+				p.pop_back();
+			}
+		}
+		else{
+			if(x!=p[pos-2].sum) lefmerge(pos-2,pos-1,pos,x);
+			else if(y!=p[pos+2].sum) rigmerge(pos,pos+1,pos+2,y);
+			else{
+				cover(p[pos-1].l,p[pos-1].r,0);
+				cover(p[pos-1].l,p[pos-1].l,-p[pos].sum);
+				p.erase(p.begin()+pos);
+				p.erase(p.begin()+pos-1);
+			}
+		}
+	}
+	return ans;
 }
+// int main(){
+// #ifdef Shun
+// 	freopen(".in","r",stdin);
+// 	freopen(".out","w",stdout);
+// #endif
+// 	ios::sync_with_stdio(false);
+// 	cin>>tc;
+// 	while(tc--){
+// 		int n;
+// 		cin>>n;
+// 		vector <ll> ans=rhythm(n,2*n,0);
+// 		cout<<"! ";
+// 		for(auto i:ans) cout<<i<<" ";
+// 		cout<<endl;
+// 	}
+// 	ET;
+// }
 //ALL FOR Zhang Junhao.
