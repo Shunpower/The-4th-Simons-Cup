5cf465fd3708defcb7c99029ca3818047c8a6758
Shunpower <81838011+Shunpower@users.noreply.github.com>
2025-03-14 13:48:36 +0800

    the data of [rhythm] added

---
+++ data/rhythm/gen.cpp
@@ -0,0 +1,194 @@
+//Author:KIT / Shunpower
+//Cloud Island & Rain Temperature
+//May the force be with you and me.
+#include <bits/stdc++.h>
+#define ET return 0
+#define fi first
+#define se second
+#define mp make_pair
+#define pb emplace_back
+#define ll long long
+#define ull unsigned long long
+#define inf INT_MAX
+#define uinf INT_MIN
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define fr1(i,a,b) for(int i=a;i<=b;i++)
+#define fr2(i,a,b) for(int i=a;i>=b;i--)
+#define ld long double
+#define il inline
+#define all(a) a.begin(),a.end()
+//Quickly power: ll d=qpow(b,p>>1,k);
+//Segment Tree: Memory Limit Excceed
+//Segment Tree: Modify()->Pushdown()
+//Mod: +M, %M, define int ll
+//Mod: Don't use 998244353 instead of 1e9+7 and so on
+//Don't solve a problem for too long time.
+using namespace std;
+const int N=1e5+10;
+namespace Shun{
+	int lowbit(int x){
+		return x&-x;
+	}
+	template <typename T>
+	inline void read(T &x){
+	   T s=0,w=1;
+	   char ch=getchar();
+	   while(ch<'0'||ch>'9'){
+			if(ch=='-'){
+				w=-1;
+			}
+			ch=getchar();
+		}
+		while(ch>='0'&&ch<='9'){
+			s=s*10+ch-'0';
+			ch=getchar();
+		}
+	   x=s*w;
+	}
+	template <typename T>
+	inline void write(T x){
+		if(x<0){
+			putchar('-');
+			x=-x;
+		}
+		if(x>9){
+			write(x/10);
+		}
+		putchar(x%10+'0');
+	}
+}
+using namespace Shun;
+
+int test=3;
+const int n=200;
+const int q=n*(n+1)/2;
+
+mt19937 rnd(time(0));
+char C[N];
+const int V=2e9+1;
+int gval(int l,int r){return (int)(rnd()%(r-l+1))+l;}
+
+vector <vector<int>> a;
+
+void valley(){
+	vector <int> ans;
+	fr1(i,1,n) ans.pb(gval(-1e9,1e9));
+	sort(ans.begin(),ans.end(),[](int &x,int &y){
+		return abs(x)<abs(y);
+	});
+	deque <int> q;
+	for(auto i:ans){
+		int op=rnd()%2;
+		if(op) q.push_back(i);
+		else q.push_front(i);
+	}
+	ans.clear();
+	for(auto i:q) ans.pb(i);
+	a.pb(ans);
+}
+void the_way_to_the_top(){
+	vector <int> ans;
+	fr1(i,1,n) ans.pb(gval(-1e9,1e9));
+	sort(ans.begin(),ans.end(),[](int &x,int &y){
+		return abs(x)>abs(y);
+	});
+	deque <int> q;
+	for(auto i:ans){
+		int op=rnd()%2;
+		if(op) q.push_back(i);
+		else q.push_front(i);
+	}
+	ans.clear();
+	for(auto i:q) ans.pb(i);
+	a.pb(ans);
+} 
+void increasing(){
+	vector <int> ans;
+	fr1(i,1,n) ans.pb(gval(-1e9,1e9));
+	sort(ans.begin(),ans.end(),[](int &x,int &y){
+		return abs(x)<abs(y);
+	});
+	a.pb(ans);
+}
+void decreasing(){
+	vector <int> ans;
+	fr1(i,1,n) ans.pb(gval(-1e9,1e9));
+	sort(ans.begin(),ans.end(),[](int &x,int &y){
+		return abs(x)>abs(y);
+	});
+	a.pb(ans);
+}
+void postive(){
+	vector <int> ans;
+	fr1(i,1,n) ans.pb(gval(0,1e9));
+	a.pb(ans);
+}
+void negative(){
+	vector <int> ans;
+	fr1(i,1,n) ans.pb(gval(-1e9,0));
+	a.pb(ans);
+}
+void pos_neg_pos(){
+	vector <int> ans;
+	fr1(i,1,n){
+		if(i&1) ans.pb(gval(0,1e9));
+		else ans.pb(gval(-1e9,0));
+	}
+	a.pb(ans);
+}
+void random(){
+	vector <int> ans;
+	fr1(i,1,n) ans.pb(gval(-1e9,1e9));
+	a.pb(ans);
+}
+void samealot(){
+	vector <int> based;
+	vector <int> ans;
+	int lim=rnd()%(n/10)+1;
+	fr1(i,1,lim) based.pb(gval(-1e9,1e9));
+	fr1(i,1,n) ans.pb(based[rnd()%lim]);
+	a.pb(ans);
+}
+void many0(){
+	vector <int> ans;
+	int lim=rnd()%(n/10)+1;
+	fr1(i,1,lim) ans.pb(0);
+	fr1(i,lim+1,n) ans.pb(gval(-1e9,1e9));
+	shuffle(ans.begin(),ans.end(),rnd);
+	a.pb(ans);
+}
+int main(){
+#ifdef Shun
+	freopen(".in","r",stdin);
+	freopen(".out","w",stdout);
+#endif
+	sprintf(C,"rhythm%d.in",test);
+	freopen(C,"w",stdout);
+	cout<<n<<" "<<q<<" "<<test<<endl;
+	valley();
+	valley();
+	the_way_to_the_top();
+	the_way_to_the_top();
+	increasing();
+	increasing();
+	decreasing();
+	decreasing();
+	postive();
+	postive();
+	negative();
+	negative();
+	pos_neg_pos();
+	pos_neg_pos();
+	random();
+	random();
+	many0();
+	many0();
+	samealot();
+	samealot();
+	for(auto i:a){
+		fr1(j,0,(int)i.size()-1) cout<<i[j]<<" \n"[j+1==i.size()];
+	}
+	ET;
+}
+//ALL FOR Zhang Junhao.
---
+++ data/rhythm/grader.cpp
@@ -0,0 +1,84 @@
+//Author:KIT / Shunpower
+//Cloud Island & Rain Temperature
+//May the force be with you and me.
+#include "rhythm.h"
+#include <stdio.h>
+#include <assert.h>
+#include <vector>
+#include <iostream>
+#include <time.h>
+namespace Shun_Li_Cheng_Zhang{
+	int tc;
+	int n,q,c;
+	std::vector <long long> tmp;
+	long long a[2001],b[2001],sum[2001];
+	long long mss[2001][2001],mssb[2001];
+	bool flg;
+	long long remind(int l,int r){
+		if(flg) return -1000000000000000000ll;
+		if(l<1||r>n||q==0||l>r){
+			flg=1;
+			std::cout<<"Wrong Answer: Invalid call"<<std::endl;
+			return -1000000000000000000ll;
+		}
+		q--;
+		return mss[l][r];
+	}
+	void main(){
+		std::cout<<"399150Shunpower,Jeremy@IJ3JF8904"<<std::endl;
+		assert(scanf("%d%d%d",&n,&q,&c)==3);
+		int orgq=q;
+		for(int _=1;_<=20;_++){
+			// std::cerr<<_<<std::endl;
+			flg=0;
+			q=orgq;
+			for(int i=1;i<=n;i++) assert(scanf("%lld",&a[i])==1); 
+			for(int i=1;i<=n;i++){
+				mss[i][i-1]=-1e18;
+				long long minn=0;
+				sum[i-1]=0;
+				for(int j=i;j<=n;j++){
+					sum[j]=sum[j-1]+a[j];
+					mss[i][j]=std::max(0ll,mss[i][j-1]);
+					mss[i][j]=std::max(mss[i][j],sum[j]-minn);
+					minn=std::min(sum[j],minn);
+				}
+			}
+			tmp=rhythm(n,q,c);
+			if(flg) return;
+			for(int i=1;i<=n;i++){
+				if(b[i]<-1000000000000000ll||b[i]>1000000000000000ll){
+					std::cout<<"Wrong Answer: Unlike Simons"<<std::endl;
+					return;
+				}
+				b[i]=tmp[i-1];
+			}
+			for(int i=1;i<=n;i++){
+				mssb[i-1]=-1e18;
+				long long minn=0;
+				sum[i-1]=0;
+				for(int j=i;j<=n;j++){
+					sum[j]=sum[j-1]+a[j];
+					mssb[j]=std::max(0ll,mssb[j-1]);
+					mssb[j]=std::max(mssb[j],sum[j]-minn);
+					minn=std::min(sum[j],minn);
+					if(mssb[j]!=mss[i][j]){
+						std::cout<<"Wrong Answer: Unlike Simons"<<std::endl;
+						return;
+					}
+				}
+			}
+			std::cout<<"OK: Simons did it"<<std::endl;
+		}
+	}
+}
+long long remind(int l,int r){
+	return Shun_Li_Cheng_Zhang::remind(l,r);
+}
+int main(){
+	freopen("rhythm.in","r",stdin);
+	freopen("rhythm.out","w",stdout);
+	Shun_Li_Cheng_Zhang::main();
+	// std::cerr<<clock()<<std::endl;
+}
+//ALL FOR Zhang Junhao.
